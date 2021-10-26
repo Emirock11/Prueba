5bd33d41da8b10f21f274098dd9215c60ccec5f0
Emiliano Pineda <emi.rock11@hotmail.com>
2021-10-25 20:17:46 -0500

    Archivos agregados

---
+++ Prueba.java
@@ -0,0 +1,11 @@
+class Test{
+    int resultado = 0;
+
+    Manzana a, b, c = new Manzana();
+
+    void metodo(int varM, float varM2, Manzana varM3){
+        String varP = new Pera();
+        Hanma Emi = new Hanma();
+    }
+
+}
